cd959eb10cc83e4b1e32a4c867aab74e564f9b7c
Dries.Van Ranst <driesvanranst94@hotmail.com>
2024-02-26 22:22:04 +0100

    Added header file for the UART print function, so it's available for other files as well. Also added a Bootloader_Init() function.

---
+++ Bootloader/Core/Inc/bootloader.h
@@ -0,0 +1,13 @@
+/*
+ * bootloader.h
+ *
+ *  Created on: Feb 26, 2024
+ *      Author: Dries Van Ranst
+ */
+
+#ifndef INC_BOOTLOADER_H_
+#define INC_BOOTLOADER_H_
+
+void Bootloader_Init(void);
+
+#endif /* INC_BOOTLOADER_H_ */
---
+++ Bootloader/Core/Src/bootloader.c
@@ -0,0 +1,15 @@
+/*
+ * bootloader.c
+ *
+ *  Created on: Feb 26, 2024
+ *      Author: Dries Van Ranst
+ */
+
+#include "bootloader.h"
+
+void Bootloader_Init()
+{
+	printf("Bootloader initialised. \r\n");
+
+}
+
