21b5912823d9f3d0611f38658530de70abf6fa5a
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2024-05-05 11:11:34 +0200

    Added SPI folder

---
+++ SPI_test/Core/Src/application1.c
@@ -0,0 +1,41 @@
+/*
+ * application1.c
+ *
+ *  Created on: Apr 26, 2024
+ *      Author: jaime
+ */
+
+#include "applications.h"
+#include "memory_operations.h"
+#include <stdio.h>
+
+
+void __attribute__((section(".custom_section1"))) application1 (void)
+{
+	printf("in app 1 \r\n");
+
+	int a = 32;
+	int b = 10;
+
+
+	for (int e =0; e<15; e++)
+	{
+		e = e +a-b;
+	}
+
+	for (int e =0; e<15; e++)
+	{
+		e = e +a-b;
+	}
+
+	for (int e =0; e<15; e++)
+	{
+		e = e +a-b;
+	}
+
+
+}
+
+
+
+
